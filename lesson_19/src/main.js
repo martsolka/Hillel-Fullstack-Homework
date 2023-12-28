2fe5cc8a57894b2a7efc7afc135c9877bbc0b3db
Solomia Martyniuk <solka.marsolka@gmail.com>
2023-12-29 00:38:39 +0200

    Homework 21 ✅

--- lesson_19/src/main.js
+++ lesson_19/src/main.js
@@ -1,6 +1,4 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
-import App from './App.vue'
+import Lesson21 from './Lesson21.vue'
 
-createApp(App).mount('#app')
+createApp(Lesson21).mount('#app')
